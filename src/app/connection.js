80a182ba927f9e298813ccc453d1968f76ec66c8
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-11-28 09:45:40 -0300

    gerando conexao

---
+++ src/app/connection.js
@@ -0,0 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+
+export class ConexaoPrisma {
+    
+    static gerarConexao(){
+        return this.prisma ? this.prisma : this.prisma = new PrismaClient()
+    }
+}
