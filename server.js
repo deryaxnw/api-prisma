a244c5a8d4f98bbb8c6887ea8f73e204a31ab2cf
Jao Ryan <joaoryanoliveira2005@gmail.com>
2024-11-28 16:08:13 -0300

    concertando as rotas

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
-import express from "express";
+import express from "express"
+const app = express()
+
 import dotenv from "dotenv";
 dotenv.config();
-import { clientRota } from "./src/router/user";
 
-const app = express()
+import { clientRota } from "./src/router/user.js";
+
 const port = process.env.PORTA;
 app.use(express.json());
-app.use("usuário", clientRota)
 app.listen(port, () => {
     console.log("meu servidor está escutando na porta " + port);
 })
+
+app.use("/criar", clientRota);
+app.use("/criar/buscar", clientRota);
--- src/router/user.js
+++ src/router/user.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
-
+import { ClientController } from "../controllers/clientController.js";
 
 export const clientRota = Router()
+clientRota.post("/", ClientController.create)
+clientRota.get("/buscar", ClientController.getAll)
